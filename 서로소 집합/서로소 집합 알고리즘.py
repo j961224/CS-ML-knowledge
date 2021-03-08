6dcc4083700d14666faed796d002bdf20f8b7020
정유석 <j961224@naver.com>
2021-03-08 17:16:20 +0900

    서로소 집합

---
+++ 서로소 집합/서로소 집합 알고리즘.py
@@ -0,0 +1,35 @@
+#서로소 집합: 공통 원소가 없는 두 집합
+
+def find_parent(parent,x):
+    if parent[x] !=x:
+        paerent[x]=find_parent(parent,parent[x])
+    return parent[x]
+
+# 두 원소가 속한 집합을 합치기 -> 작은 쪽으로 합치기
+def union_parent(parent,a,b):
+    a=find_parent(parent,a)
+    b=find_parent(parent,b)
+    if a<b:
+        parent[b]=a
+    else:
+        parent[a]=b
+
+v,e = map(int,input().split())
+parent=[0]*(v+1)
+
+for i in range(1,v+1):
+    parent[i]=i
+    
+for i in range(e):
+    a,b=map(int,input().split())
+    union_parent(parent,a,b)
+
+print('각 원소가 속한 집합: ', end='')
+for i in range(1,v+1):
+    print(find_parent(parent,i),end=' ')
+
+print()
+
+print('부모 테이블: ',end='')
+for i in range(1,v+1):
+    print(parent[i],end=' ')
